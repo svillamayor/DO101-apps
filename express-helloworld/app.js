25f9a01753b93534cfa2d62541f9767ce5268a54
Sebastian Villamayor <svillamayor@ute.com.uy>
2020-06-05 09:15:24 -0300

    Port changed

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,7 +9,7 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
-app.listen(88, function () {
-  console.log('Example app listening on port 88!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
